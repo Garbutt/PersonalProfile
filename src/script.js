ee566ba6c7d385c3418b056cb81beb50714949ab
Reece Garbutt <reecegarbutt2017@gmail.com>
2023-12-26 23:10:04 +0000

    Personal Profile Update

---
+++ src/script.js
@@ -0,0 +1,9 @@
+/*open nav*/
+function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/*close nav*/
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
